60b42105e91ddeead9dad9548c093fa50e04b402
imratsingh <162085150+imratsingh@users.noreply.github.com>
2026-02-27 12:47:15 +0530

    added more pattern

--- for_loop.py
+++ for_loop.py
@@ -1,4 +1,4 @@
-# ---------------------------------------- for loop ---------------------------------
+# ---------------------------------------- for loop ---------------------------------5
 # *
 # * *
 # * * *
@@ -14,23 +14,68 @@
 # * * * *
 # * * * *
 # * * * *
-n = int(input("Enter your number :"))
-for i in range(n):
-    for j in range(n):
-        print("*",end=" ")
-    print()
+
+# n = int(input("Enter your number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print("*",end=" ")
+#     print()
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# n = int(input("Enter your number :"))
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+
+# n = int(input("Enter your number :"))
+# for i in range(1,n+1):
+#     print(' '*(n-i),end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+
+# n = int(input("Enter your number :"))
+# for i in range(1,n+1):
+#     print(' '*(n-i),end="")
+#     for i in range(i):
+#         print('*',end="")
+#     print()
+
 # 1 
 # 1 1 
 # 1 1 1 
 # 1 1 1 1 
 # 1 1 1 1 1
+
 # n = int(input("Enter your number :"))
 # for i in range(1,n+1):
-#     x =1
-#     for j in range(1,i+1):
-#         print(x,end=' ')
+#     for i in range(1,i+1):
+#         print("1",end=" ")
 #     print()
 
+# n = int(input("Enter your number :"))
+# for i in range(1,n+1):
+#     x = 1
+#     for j in range(1,i+1):
+#         print(x,end=" ")
+#     print()
 
 # 3 
 # 3 6 
@@ -41,7 +86,52 @@ for i in range(n):
 # n = int(input("Enter your number :"))
 # for i in range(1,n+1):
 #     x = 3
-#     for i in range(1,i + 1):
-#         print(x,end=' ')
-#         x = x+3
+#     for j in range(1,i+1):
+#         print(x,end=" ")
+#         x = x + 3
 #     print()
+# -----------------------------------------------------------------------
+# ✔ ord() → character to integer
+# ✔ chr() → integer to character
+
+# A
+# A B 
+# A B C 
+# A B C D 
+# A B C D E
+
+# n = int(input("Enter your number :"))
+# for i in range(1,n+1):
+#     ch = "A"
+#     for j in range(i):
+#         print(ch,end=" ")
+#         ch = chr(ord(ch)+1)
+#     print()
+
+# A
+# A B 
+# A B C
+# A B C D
+# A B C D E 
+
+# n = int(input("Enter your number :"))
+# for i in range(1,n+1):
+#     ch = "A"
+#     for j in range(1,i+1):
+#         print(ch,end=" ")
+#         ch = chr(ord(ch)+ 1)
+#     print()
+
+# A 
+# A C 
+# A C E 
+# A C E G 
+# A C E G H 
+
+n = int(input("Enter your number :"))
+for i in range(1,n+1):
+    ch = "A"
+    for j in range(1,i+1):
+        print(ch,end=" ")
+        ch = chr(ord(ch)+2)
+    print()
